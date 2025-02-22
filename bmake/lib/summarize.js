b41fd307129b5848ea589326914e0ddb6de1fca7
Sonny Chen <chensonny46@gmail.com>
2025-02-22 11:32:57 -0500

    added translate/summarize to library. needs revision

--- bmake/lib/summarize.js
+++ bmake/lib/summarize.js
@@ -1,6 +1,8 @@
 import OpenAI from "openai";
-const openai = new OpenAI();
 
+const openai = new OpenAI(
+    process.env.OPENAI_API_KEY
+);
 
 async function summarizeText(text) {
     const completion = await openai.chat.completions.create({
