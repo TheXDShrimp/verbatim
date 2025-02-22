9a63edde44900a762e60891ef886d403b84a0515
ElemelonWind <c.yang.2023@gmail.com>
2025-02-22 18:24:13 -0500

    minor ui changes

--- bmake/pages/dashboard.js
+++ bmake/pages/dashboard.js
@@ -48,7 +48,7 @@ export default function Dashboard({ user }) {
       <div className="absolute top-0 left-0 w-screen">
       <nav className="flex items-center justify-between p-8">
         <div className="flex items-center space-x-4">
-          <h1 className="text-2xl font-bold">Speakerize</h1>
+        <Link href="/" className="text-2xl font-bold">Speakerize</Link>
         </div>
         <div className="flex items-center space-x-8">
             <Link href="/dashboard" className="text-lg font-semibold hover:text-gray-300">Dashboard</Link>
